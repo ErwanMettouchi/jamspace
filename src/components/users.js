fa608f71f870cf3a6cf696dc788f7b30cd1e5ae6
ErwanMettouchi <erwan.mettouchi@yahoo.com>
2023-07-27 07:17:45 +0200

    Menu déroulant affichant uniquement les villes des utilisateurs, ajout d'une fonction affichant les utilisateurs venant d'une ville en particulier, pas encore fonctionnelle.

--- src/components/users.js
+++ src/components/users.js
@@ -2,27 +2,7 @@ import React, { useState, useEffect } from 'react'
 import data from '../data.json'
 import axios from 'axios'
 
-
-function handleClick(list) {
-    
-}
-
-export default function Users() {
-
-    const [departements, setDepartements] = useState([])
-    const dataUsers = data.map((users) => {
-        return {...users, departments: users.departments.filter((dep) => dep === departements.map((departement) => departement.code))}
-    })
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await axios('https://geo.api.gouv.fr/departements')
-            setDepartements(...departements, result.data)
-        }
-        fetchData()
-    }, [])
-    
-    console.log(dataUsers)
+export default function DisplayUsers() {
 
     return (
         <div>
@@ -31,9 +11,36 @@ export default function Users() {
                     <li key={users.id}>{users.name}</li>
                 )}
             </ul>
-            <select>
-                    
-            </select>
+            <Dropdown />
         </div>
     )
+}
+
+function Dropdown() {
+
+    const [departements, setDepartements] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios('https://geo.api.gouv.fr/departements')
+            setDepartements(...departements, result.data)
+        }
+        fetchData()
+    }, [])
+
+    const departmentsUsers = data.flatMap(user => user.departments)
+    const filteredDepartments = departements.flatMap(d => d.code).filter((department) => departmentsUsers.includes(parseInt(department)))
+
+    // console.log(filteredDepartments)
+
+    return (
+        <select style={{padding: 10, textAlign: "left", margin: 15}} onChange={DisplayUsersOnClick}>
+            {departements.map(dep => filteredDepartments.includes(dep.code) ? <option value={dep.code}>{dep.nom}</option> : null)}
+        </select>
+    )
+}
+
+function DisplayUsersOnClick(code) {
+    return (
+        data.map(user => user.departments).filter(u => u.includes(parseInt(code)) ? <li key={u.id>u.name}>{u.name}</li> : null))
 }
